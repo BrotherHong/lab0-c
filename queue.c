cf81496d9fffba981e82ea87071ed470e2237279
BrotherHong <leo361288@gmail.com>
2025-02-26 22:00:46 +0800

    Implement q_insert_tail in queue.c
    This function insert an element at the tail.
    It checks if the allocation of element is success or not,
    then initialize the element with specified string before
    adding this element to the queue's tail.
    
    Change-Id: I1f578b4e7792a8b8762653c931b6dcf0a41b159a

--- queue.c
+++ queue.c
@@ -61,6 +61,21 @@ bool q_insert_head(struct list_head *head, char *s)
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+
+    element_t *e = NULL;
+    if (!(e = malloc(sizeof(element_t))))
+        return false;
+
+    INIT_LIST_HEAD(&e->list);
+    size_t len = strlen(s);
+    if (!(e->value = malloc(sizeof(char) * (len + 1))))
+        return false;
+    strncpy(e->value, s, len);
+    e->value[len] = '\0';
+    list_add_tail(&e->list, head);
+
     return true;
 }
 
