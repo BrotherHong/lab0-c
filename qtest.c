7b9debb7a6c1e409cca06b053f2c311443b51502
BrotherHong <leo361288@gmail.com>
2025-03-10 20:10:56 +0800

    Add shuffle command in qtest
    Implement Fisher-Yates shuffle for this command.
    The algorithm picks a random node from the list
    and swap it with the last unpicked node, which
    can produce an unbiased permutation.
    
    Change-Id: I321a170225d129e53b3bf790e7791c409053bd0c

--- qtest.c
+++ qtest.c
@@ -1056,6 +1056,55 @@ static bool do_next(int argc, char *argv[])
     return q_show(0);
 }
 
+void q_shuffle(struct list_head *head)
+{
+    int len = q_size(head);
+    struct list_head *new = head->prev;
+
+    while (len) {
+        int random = rand() % len;
+
+        struct list_head *old = head->next;
+        while (random--) {
+            old = old->next;
+        }
+
+        /* Swap value */
+        element_t *old_entry = list_entry(old, element_t, list);
+        element_t *new_entry = list_entry(new, element_t, list);
+        char *tmp = old_entry->value;
+        old_entry->value = new_entry->value;
+        new_entry->value = tmp;
+
+        len--;
+        new = new->prev;
+    }
+}
+
+static bool do_shuffle(int argc, char *argv[])
+{
+    if (argc != 1) {
+        report(1, "%s takes no arguments", argv[0]);
+        return false;
+    }
+
+    if (!current || !current->q) {
+        report(3, "Warning: Try to access null queue");
+        return false;
+    }
+    error_check();
+
+    set_noallocate_mode(true);
+    if (exception_setup(true))
+        q_shuffle(current->q);
+    exception_cancel();
+
+    set_noallocate_mode(false);
+
+    q_show(3);
+    return !error_check();
+}
+
 static void console_init()
 {
     ADD_COMMAND(new, "Create new queue", "");
@@ -1096,6 +1145,7 @@ static void console_init()
                 "");
     ADD_COMMAND(reverseK, "Reverse the nodes of the queue 'K' at a time",
                 "[K]");
+    ADD_COMMAND(shuffle, "Shuffle the nodes of the queue", "");
     add_param("length", &string_length, "Maximum length of displayed string",
               NULL);
     add_param("malloc", &fail_probability, "Malloc failure probability percent",
